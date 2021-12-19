bed29e251b93b3720379be2ffae76448b2013776
shaimaa-elsayed <96368871+shaimaa-elsayed@users.noreply.github.com>
2021-12-20 02:36:31 +0300

    set up

--- backEnd/Server.js
+++ backEnd/Server.js
@@ -1,10 +1,27 @@
 const express =require ("express")
 const app = express()
 const Datebase  = require ("./Datebase")
-app.get('/',(req,res)=>{
-res.json('get/ is working')
+const Todo = require ("./todo")
 
+app.get("/",(req,res)=>{
+
+
+    res.json("get is working")
+});
+
+app.get("/tasks",(req,res)=>{
+Todo.find({},(error,date)=>{
+if (error)
+{console.log("error::", error);
+
+}else{
+
+
+res.json(date);
+}
 })
+});
+
 
 app.listen(3000,()=>{
 
